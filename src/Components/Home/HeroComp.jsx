e3edc905eaf9916f3d47d11a0742706e87fd7166
fauzan18296 <fauzan18296@gmail.com>
2024-01-24 21:00:07 +0700

    style:menambahkan background carousel & merubah padding pada image

--- src/Components/Home/HeroComp.jsx
+++ src/Components/Home/HeroComp.jsx
@@ -1,48 +1,145 @@
 import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import { Typewriter } from "react-simple-typewriter";
 const HeroSection = () => {
-  const [changeText, setChangeText] = useState(["Luxury", "ASAF"]);
+  const [changeText, setChangeText] = useState(["Luxury", "Various"]);
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
 
   return (
-    <div className="Hero-bg d-flex justify-content-center align-items-center">
-      <Container
-        className="fs-1 d-flex align-items-center"
-        style={{
-          letterSpacing: "1px",
-          lineHeight: "50px",
-        }}
-      >
-        <Row>
-          <Col className="fs-1 ms-1 contain">
-            <div
-              className="text-uppercase luxury"
-              style={{
-                color: "#9C881E",
-                fontSize: "1.4em",
-              }}
-            >
-              <p className="luxury">
-                <Typewriter
-                  words={changeText}
-                  loop={false}
-                  cursorStyle={"_"}
-                  cursor
-                />
-              </p>
-            </div>
-            <div
-              className="text-capitalize ms-5 fashion"
-              style={{
-                fontSize: "1.7em",
-              }}
-            >
-              <p className="fashion">Fashion</p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Carousel
+      activeIndex={index}
+      onSelect={handleSelect}
+      data-bs-theme="dark"
+      interval={500}
+      fade
+    >
+      <Carousel.Item interval={1000}>
+        <div className="Hero-bg1 d-flex justify-content-center align-items-center">
+          <Container
+            className="fs-1 d-flex align-items-center"
+            style={{
+              letterSpacing: "1px",
+              lineHeight: "50px",
+            }}
+          >
+            <Row>
+              <Col className="fs-1 ms-1 contain">
+                <div
+                  className="text-uppercase luxury"
+                  style={{
+                    color: "#9C881E",
+                    fontSize: "1.4em",
+                  }}
+                >
+                  <p className="luxury">
+                    <Typewriter
+                      words={changeText}
+                      loop={false}
+                      cursorStyle={"_"}
+                      cursor
+                    />
+                  </p>
+                </div>
+                <div
+                  className="text-capitalize ms-5 fashion"
+                  style={{
+                    fontSize: "1.7em",
+                  }}
+                >
+                  <p className="fashion">Fashion</p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div className="Hero-bg2 d-flex justify-content-center align-items-center">
+          <Container
+            className="fs-1 d-flex align-items-center"
+            style={{
+              letterSpacing: "1px",
+              lineHeight: "50px",
+            }}
+          >
+            <Row>
+              <Col className="fs-1 ms-1 contain">
+                <div
+                  className="text-uppercase luxury"
+                  style={{
+                    color: "#9C881E",
+                    fontSize: "1.4em",
+                  }}
+                >
+                  <p className="luxury">
+                    <Typewriter
+                      words={changeText}
+                      loop={false}
+                      cursorStyle={"_"}
+                      cursor
+                    />
+                  </p>
+                </div>
+                <div
+                  className="text-capitalize ms-5 fashion"
+                  style={{
+                    fontSize: "1.7em",
+                  }}
+                >
+                  <p className="fashion">Fashion</p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div className="Hero-bg3 d-flex justify-content-center align-items-center">
+          <Container
+            className="fs-1 d-flex align-items-center"
+            style={{
+              letterSpacing: "1px",
+              lineHeight: "50px",
+            }}
+          >
+            <Row>
+              <Col className="fs-1 ms-1 contain">
+                <div
+                  className="text-uppercase luxury"
+                  style={{
+                    color: "#9C881E",
+                    fontSize: "1.4em",
+                  }}
+                >
+                  <p className="luxury">
+                    <Typewriter
+                      words={changeText}
+                      loop={false}
+                      cursorStyle={"_"}
+                      cursor
+                    />
+                  </p>
+                </div>
+                <div
+                  className="text-capitalize ms-5 fashion"
+                  style={{
+                    fontSize: "1.7em",
+                  }}
+                >
+                  <p className="fashion">Fashion</p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 export default HeroSection;
