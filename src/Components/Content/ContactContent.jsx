ad284a2219885523e396df1885a4a41fbb7fe601
fauzan18296 <fauzan18296@gmail.com>
2024-01-31 20:49:06 +0700

    style:Merubah waktu animasi di bagian page about dan membuat layout input-di-page-contact

--- src/Components/Content/ContactContent.jsx
+++ src/Components/Content/ContactContent.jsx
@@ -1,41 +1,48 @@
 import { Container, Row, Col, Form, InputGroup } from "react-bootstrap";
-import InputGroupText from "react-bootstrap/esm/InputGroupText";
+
 const ContactContent = () => {
   return (
     <div>
       <Container className="contact-page d-flex justify-content-center align-items-center">
-        <Row className="d-flex justify-content-center align-items-center mx-auto">
-          <Col className="mx-auto">
-            <h1>Page Contact</h1>
+        <Row className="d-flex justify-content-center align-items-center mx-3 my-3">
+          <Col className="mx-3 my-3 w-full">
             <Form inline>
-              <InputGroup size="lg" className="my-3 ">
+              <InputGroup
+                size="lg"
+                className="d-flex justify-content-center align-items-center my-3 "
+              >
                 <Form.Label className="d-flex align-items-center pe-4 fs-4 fw-semibold">
-                  <div>Username :</div>
+                  <div>Username:</div>
+                  <Form.Control
+                    type="text"
+                    placeholder="username"
+                    className="d-flex align-items-center input-control text-capitalize ms-3 "
+                  />
                 </Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="username"
-                  className="d-flex align-items-center input-control text-capitalize "
-                />
               </InputGroup>
-              <InputGroup size="lg" className="my-3">
+
+              <InputGroup
+                size="xl"
+                className="d-flex justify-content-center align-items-center my-3"
+              >
                 <Form.Label className="d-flex align-items-center pe-4 fs-4 fw-semibold">
-                  <div>Password :</div>
+                  <div>Password:</div>
+                  <Form.Control
+                    type="password"
+                    placeholder="password"
+                    className="d-flex align-items-center input-control text-capitalize ms-3"
+                  />
                 </Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="password"
-                  className="d-flex align-items-center input-control text-capitalize"
-                />
               </InputGroup>
-              <InputGroup size="lg" className="my-3">
-                <Form.Label className="d-flex align-items-center pe-4 fs-4 fw-semibold">
-                  <div>Suggestion :</div>
+
+              <InputGroup size="lg" className="d-block">
+                <Form.Label className="d-flex pe-4  fs-4 fw-semibold">
+                  <div>Suggestion:</div>
                 </Form.Label>
                 <Form.Control
                   type="text"
                   placeholder="suggestion"
-                  className="d-flex align-items-center input-control text-capitalize"
+                  className="d-flex px-2 justify-content-center input-suggestion text-capitalize"
                 />
               </InputGroup>
             </Form>
