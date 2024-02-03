0b527295fde68588b5b4efe7f3e556d2b276257c
fauzan18296 <fauzan18296@gmail.com>
2024-02-03 17:06:50 +0700

    feat: menambahkan icon sosmed di bagian about-page

--- src/Components/Content/CatalogContent.jsx
+++ src/Components/Content/CatalogContent.jsx
@@ -3,7 +3,13 @@ import { Container, Col, Row } from "react-bootstrap";
 const CatalogContent = () => {
   return (
     <div>
-      <h1 className="m-5">Page Catalog</h1>
+      <Container className="catalog-content d-flex justify-content-center align-items-center w-full">
+        <Row>
+          <Col>
+            <h1 className="m-5">Page Catalog</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
